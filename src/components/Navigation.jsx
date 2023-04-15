d022ae0c60b48931814c6f0b080799d2e3c07e03
Andrea <andrearivas4cinfor@gmail.com>
2023-04-15 14:16:33 +0200

    add scss

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Spin as Hamburger } from 'hamburger-react';
 import { Link } from 'react-router-dom';
 import Logo from '../assets/icons/logo.png';
+import '../styles/navigation.scss';
 
 const Navigation = () => {
 const [isOpen, setOpen] = useState(false);
