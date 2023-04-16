3e88c93c5ebc5e2ab375f94e28b8c063df01d224
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-16 10:23:43 +0530

    linter fixed and dlt default props

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,51 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import bannerImage from '../../assets/banner/banner.jpg';
 import '../../styles/banner.scss';
 
-const Banner = ({title, description}) => {
-  //  i will use this in future 
-  // const [scrollPosition, setScrollPosition] = useState(0);
-
-  // const handleScroll = () => {
-  //   const position = window.pageYOffset;
-  //   setScrollPosition(position);
-  // };
-
-  // useEffect(() => {
-  //   window.addEventListener('scroll', handleScroll, { passive: true });
-  //   return () => {
-  //     window.removeEventListener('scroll', handleScroll);
-  //   };
-  // }, []);
-
-  // const parallaxStyle = {
-  //   transform: `translateY(-${scrollPosition * 0.3}px)`,
-  //   backgroundImage: `url(${bannerImage})`,
-  // };
-
-  return (
-    <section className="banner">
-      <img src={bannerImage} alt="banner-image" />
-      <div className="banner-content">
-        <h1>{title}</h1>
-        {/*  */}
-        <p>
-          {description}
-        </p>
-        <button>our menu</button>
-      </div>
-    </section>
-  );
-};
-
+const Banner = ({ title, description }) => (
+  <section className="banner">
+    <img src={bannerImage} alt="banner" />
+    <div className="banner-content">
+      <h1>{title}</h1>
+      {/*  */}
+      <p>
+        {description}
+      </p>
+      <button type="button">our menu</button>
+    </div>
+  </section>
+);
 Banner.propTypes = {
   title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired
-}
-
-Banner.defaultProps = {  
-  title : "Afgan Resturent",
-  description : "we serve you well",
-}  
+  description: PropTypes.string.isRequired,
+};
 
 export default Banner;
+
+//  i will use this in future
+// const [scrollPosition, setScrollPosition] = useState(0);
+
+// const handleScroll = () => {
+//   const position = window.pageYOffset;
+//   setScrollPosition(position);
+// };
+
+// useEffect(() => {
+//   window.addEventListener('scroll', handleScroll, { passive: true });
+//   return () => {
+//     window.removeEventListener('scroll', handleScroll);
+//   };
+// }, []);
+
+// const parallaxStyle = {
+//   transform: `translateY(-${scrollPosition * 0.3}px)`,
+//   backgroundImage: `url(${bannerImage})`,
+// };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,14 @@
-import Banner from "../components/banner/Banner";
+import Banner from '../components/banner/Banner';
 
 const HomePage = () => {
-    const description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit a pariatur asperiores optio libero officiis repudiandae amet aspernatur, omnis "
-    const title = "anything we want "
-return(
+  const description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit a pariatur asperiores optio libero officiis repudiandae amet aspernatur, omnis ';
+  const title = 'anything we want ';
+  return (
     <main>
-        <Banner description={description} title={title} />
-        <h1>Home Page</h1>
+      <Banner description={description} title={title} />
+      <h1>Home Page</h1>
     </main>
-)};
+  );
+};
 
 export default HomePage;
