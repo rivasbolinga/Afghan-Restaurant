09adafb7186c45457cff377307660b711a755b23
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-04-19 17:45:14 +0200

    Create Testimonials file and import and export to Home Page component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,12 @@
 import Banner from '../components/banner/Banner';
-
+import Testimonials from '../components/Testimonials';
 const HomePage = () => {
   const description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit a pariatur asperiores optio libero officiis repudiandae amet aspernatur, omnis ';
   const title = 'anything we want ';
   return (
     <main>
       <Banner description={description} title={title} />
-      <h1>Home Page</h1>
+      <Testimonials />
     </main>
   );
 };
