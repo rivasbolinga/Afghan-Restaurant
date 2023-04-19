b758f622d6c929c0c82b6ca7b9ef17234b51a05e
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-04-19 19:19:53 +0200

    Try to center elements

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -5,6 +5,7 @@ import 'slick-carousel/slick/slick-theme.css';
 import MagicSliderDots from 'react-magic-slider-dots';
 import 'react-magic-slider-dots/dist/magic-dots.css';
 import '../styles/testimonials.scss';
+import Umar from '../assets/images/Umar.png';
 
 class Testimonials extends Component {
   render() {
@@ -24,40 +25,52 @@ class Testimonials extends Component {
       <div className="slider-container">
         <Slider className="slider" {...settings}>
           <div className="testimonial-container">
-            <h3 className='testimonial-text'>
+            <h3 className="testimonial-text">
               "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
               ad rerum odio doloribus minima inventore atque, eveniet placeat
               ipsum. Eius harum ratione, fugiat iste officia praesentium non
               similique sed magni?"
             </h3>
             <div className="testimonial-info">
-              <img className="testimonial-image" src="testimonial"></img>
+              <img
+                className="testimonial-image"
+                alt="testimonial"
+                src={Umar}
+              ></img>
               <h4 className="testimonial-name">UMAR SHINWARI</h4>
               <p className="testimonial-position">CEO, Founder</p>
             </div>
           </div>
           <div className="testimonial-container">
-            <h3>
+            <h3 className="testimonial-text">
               "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
               ad rerum odio doloribus minima inventore atque, eveniet placeat
               ipsum. Eius harum ratione, fugiat iste officia praesentium non
               similique sed magni?"
             </h3>
             <div className="testimonial-info">
-              <img className="testimonial-image" src="testimonial"></img>
+              <img
+                className="testimonial-image"
+                alt="testimonial"
+                src={Umar}
+              ></img>
               <h4 className="testimonial-name">UMAR SHINWARI</h4>
               <p className="testimonial-position">CEO, Founder</p>
             </div>
           </div>
           <div className="testimonial-container">
-            <h3>
+            <h3 className="testimonial-text">
               "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
               ad rerum odio doloribus minima inventore atque, eveniet placeat
               ipsum. Eius harum ratione, fugiat iste officia praesentium non
               similique sed magni?"
             </h3>
             <div className="testimonial-info">
-              <img className="testimonial-image" src="testimonial"></img>
+              <img
+                className="testimonial-image"
+                alt="testimonial"
+                src={Umar}
+              ></img>
               <h4 className="testimonial-name">UMAR SHINWARI</h4>
               <p className="testimonial-position">CEO, Founder</p>
             </div>
