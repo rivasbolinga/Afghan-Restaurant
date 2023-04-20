d65cf86d8de6b014de0d8bdfa319ac8e7d0b6734
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-04-20 10:15:34 +0200

    Finish style

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -7,78 +7,73 @@ import 'react-magic-slider-dots/dist/magic-dots.css';
 import '../styles/testimonials.scss';
 import Umar from '../assets/images/Umar.png';
 
-class Testimonials extends Component {
-  render() {
-    const settings = {
-      dots: true,
-      arrows: true,
-      infinite: false,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      appendDots: (dots) => {
-        return <MagicSliderDots dots={dots} numDotsToShow={3} dotWidth={30} />
-      },
-    }
+const Testimonials = () => {
+  const settings = {
+    dots: true,
+    arrows: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    appendDots: (dots) => (
+      <MagicSliderDots dots={dots} numDotsToShow={3} dotWidth={30} />
+    ),
+  };
 
-    return (
-      <div className="slider-container">
-        <Slider className="slider" {...settings}>
-          <div className="testimonial-container">
-            <h3 className="testimonial-text">
-              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-              ad rerum odio doloribus minima inventore atque, eveniet placeat
-              ipsum. Eius harum ratione, fugiat iste officia praesentium non
-              similique sed magni?"
-            </h3>
-            <div className="testimonial-info">
-              <img
-                className="testimonial-image"
-                alt="testimonial"
-                src={Umar}
-              ></img>
-              <h4 className="testimonial-name">UMAR SHINWARI</h4>
-              <p className="testimonial-position">CEO, Founder</p>
-            </div>
+  return (
+    <div className="slider-container">
+      <Slider
+        className="slider"
+        dots={settings.dots}
+        arrows={settings.arrows}
+        infinite={settings.infinite}
+        speed={settings.speed}
+        slidesToShow={settings.slidesToShow}
+        slidesToScroll={settings.slidesToScroll}
+        appendDots={settings.appendDots}
+      >
+        <div className="testimonial-container">
+          <h3 className="testimonial-text">
+            &quot;Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Aliquid ad rerum odio doloribus minima inventore atque, eveniet
+            placeat ipsum. Eius harum ratione, fugiat iste officia praesentium
+            non similique sed magni?&quot;
+          </h3>
+          <div className="testimonial-info">
+            <img className="testimonial-image" alt="testimonial" src={Umar} />
+            <h4 className="testimonial-name">UMAR SHINWARI</h4>
+            <p className="testimonial-position">CEO, Founder</p>
           </div>
-          <div className="testimonial-container">
-            <h3 className="testimonial-text">
-              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-              ad rerum odio doloribus minima inventore atque, eveniet placeat
-              ipsum. Eius harum ratione, fugiat iste officia praesentium non
-              similique sed magni?"
-            </h3>
-            <div className="testimonial-info">
-              <img
-                className="testimonial-image"
-                alt="testimonial"
-                src={Umar}
-              ></img>
-              <h4 className="testimonial-name">UMAR SHINWARI</h4>
-              <p className="testimonial-position">CEO, Founder</p>
-            </div>
+        </div>
+        <div className="testimonial-container">
+          <h3 className="testimonial-text">
+            &quot;Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Aliquid ad rerum odio doloribus minima inventore atque, eveniet
+            placeat ipsum. Eius harum ratione, fugiat iste officia praesentium
+            non similique sed magni?&quot;
+          </h3>
+          <div className="testimonial-info">
+            <img className="testimonial-image" alt="testimonial" src={Umar} />
+            <h4 className="testimonial-name">UMAR SHINWARI</h4>
+            <p className="testimonial-position">CEO, Founder</p>
           </div>
-          <div className="testimonial-container">
-            <h3 className="testimonial-text">
-              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-              ad rerum odio doloribus minima inventore atque, eveniet placeat
-              ipsum. Eius harum ratione, fugiat iste officia praesentium non
-              similique sed magni?"
-            </h3>
-            <div className="testimonial-info">
-              <img
-                className="testimonial-image"
-                alt="testimonial"
-                src={Umar}
-              ></img>
-              <h4 className="testimonial-name">UMAR SHINWARI</h4>
-              <p className="testimonial-position">CEO, Founder</p>
-            </div>
+        </div>
+        <div className="testimonial-container">
+          <h3 className="testimonial-text">
+            &quot;Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Aliquid ad rerum odio doloribus minima inventore atque, eveniet
+            placeat ipsum. Eius harum ratione, fugiat iste officia praesentium
+            non similique sed magni?&quot;
+          </h3>
+          <div className="testimonial-info">
+            <img className="testimonial-image" alt="testimonial" src={Umar} />
+            <h4 className="testimonial-name">UMAR SHINWARI</h4>
+            <p className="testimonial-position">CEO, Founder</p>
           </div>
-        </Slider>
-      </div>
-    )
-  }
-}
+        </div>
+      </Slider>
+    </div>
+  );
+};
 
 export default Testimonials;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import Banner from '../components/banner/Banner';
 import Testimonials from '../components/Testimonials';
+
 const HomePage = () => {
   const description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit a pariatur asperiores optio libero officiis repudiandae amet aspernatur, omnis ';
   const title = 'anything we want ';
